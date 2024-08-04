5ba52709b036ab97dacbd218d2b21017856d3aea
Dania Jawad <djawad02@gmail.com>
2024-08-04 15:22:24 +0500

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,6 +18,9 @@ export interface Game {
   }
   
   
-const useGames = (gameQuery:GameQuery) => useData<Game>('/games',{params:{genres:gameQuery.genre?.id,platform:gameQuery.platform?.id}},[gameQuery])
+const useGames = (gameQuery:GameQuery) => useData<Game>('/games',
+  {
+    params:{genres:gameQuery.genre?.id,platform:gameQuery.platform?.id,ordering:gameQuery.sortOrder}
+  },[gameQuery])
 
 export default useGames;
