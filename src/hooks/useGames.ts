dcd3fa865358443a247c3ac31cc18f1d8695108e
Dania Jawad <djawad02@gmail.com>
2024-08-04 16:34:03 +0500

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,7 +14,8 @@ export interface Game {
     background_image:string;
     //object which has type plaform
     parent_platforms: {platform:Platform}[];
-    metacritic: number
+    metacritic: number;
+    rating_top:number;
   }
   
   
