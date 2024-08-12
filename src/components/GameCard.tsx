8d6b62156163e92496793ffcdfe1e8ee2cf72cb5
Dania Jawad <djawad02@gmail.com>
2024-08-12 12:21:15 +0500

    Refactor entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Game } from "../entities/Game";
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import Game from "../entities/Game";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageURL from "../services/image-url";
